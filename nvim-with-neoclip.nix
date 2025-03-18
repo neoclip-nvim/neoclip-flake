89ac281a332819a7cad9f1976469cb67b5b7e6cc
Slava <visla.vvi@gmail.com>
2025-03-18 16:12:35 +0500

    (broken) autologin+session

--- nvim-with-neoclip.nix
+++ nvim-with-neoclip.nix
@@ -6,6 +6,7 @@ pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
     plugin = pkgs.vimPlugins.neoclip;
     config = /* vim */ ''
       lua require('neoclip'):setup()
+      vim.cmd('checkhealth neoclip | w! /tmp/health | qa!')
     '';
   }];
 }
--- test.nix
+++ test.nix
@@ -5,18 +5,31 @@ testers.runNixOSTest {
   nodes.machine = { config, pkgs, ... }:
   let
     nvim = pkgs.callPackage ./nvim-with-neoclip.nix {};
+    session = {
+      manage = "desktop";
+      name = "nvim-test";
+      start = ''
+        ${pkgs.neovim}/bin/nvim &
+        waitPID=$!
+        '';
+      };
   in
   {
     environment.systemPackages = [nvim];
 
     services.xserver.enable = true;
+    services.xserver.displayManager.session = [
+      session
+    ];
     services.displayManager.sddm.enable = true;
     services.xserver.desktopManager.xterm.enable = true;
     services.displayManager.autoLogin.enable = true;
     services.displayManager.autoLogin.user = "alice";
+    services.displayManager.defaultSession = "xterm";
 
     users.users.alice = {
       isNormalUser = true;
+      password = "";
       extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
         uid = 1000;
     };
@@ -26,7 +39,6 @@ testers.runNixOSTest {
 
   testScript = /* python */ ''
     machine.start()
-    machine.send_chars("root\n\n")
     # machine.succeed('nvim')
   '';
 }
